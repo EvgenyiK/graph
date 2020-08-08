1b6005d3fc2f8bc0ad34076c67b662af61026025
Evgen <evgenprogrammer@gmail.com>
2020-08-08 16:16:53 +0300

    очепятка

--- models/models.go
+++ models/models.go
@@ -3,6 +3,6 @@ package models
 //GraphNode ...
 type GraphNode struct {
     ID        int64 `json:"id"`
-    Neighbors int64 `json:"neighboors"`
+    Neighbors int64 `json:"neighbors"`
     Roots     int64 `json:"roots"`
 }
