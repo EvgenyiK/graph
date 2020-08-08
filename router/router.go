d545c7839d3bf7973d5528d1446292c1e420da73
Evgen <evgenprogrammer@gmail.com>
2020-08-08 15:16:56 +0300

    маршрутизация

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import(
+	"github.com/EvgenyiK/graph/midleware"
+	"github.com/gorilla/mux"
+)
+
+//Router маршрутизация
+func Router() *mux.Router {
+	router:= mux.NewRouter()
+
+	router.HandleFunc("/api/graph/{id}", midleware.GetGraph).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/graph", midleware.GetAllGraph).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newgraph", midleware.CreateGraph).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/graph/{id}", midleware.UpdateGraph).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deletegraph/{id}", midleware.DeleteGraph).Methods("DELETE", "OPTIONS")
+
+	return router
+}
