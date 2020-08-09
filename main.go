1f10344eecdc8b33092c98a95d3bbb4d2dd1fb1b
Evgen <evgenprogrammer@gmail.com>
2020-08-09 14:43:19 +0300

    вывод графов в консоль

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/EvgenyiK/graph/midleware"
 	"github.com/EvgenyiK/graph/router"
 )
 
 func main() {
 	r := router.Router()
+	midleware.TestAdd()
 	fmt.Println("Starting server on the port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
