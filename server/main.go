11ebfda230d10c79ca62a7c174eb19b223759dd7
Evgen <evgenprogrammer@gmail.com>
2020-08-12 18:31:26 +0300

    наброски на реакте

--- server/main.go
+++ server/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	r := router.Router()
-	midleware.TestAdd()
+    midleware.TestAdd()
 	fmt.Println("Starting server on the port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
